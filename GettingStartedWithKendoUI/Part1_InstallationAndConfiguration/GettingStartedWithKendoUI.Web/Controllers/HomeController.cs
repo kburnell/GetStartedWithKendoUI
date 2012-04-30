c26e3fe70c4d505123c16d5939f58b46921cde6d
Keith Burnell <keith@dotnetdevdude.com>
2012-04-30 13:43:24 -0500

    Initial code push.

---
+++ GettingStartedWithKendoUI/Part1_InstallationAndConfiguration/GettingStartedWithKendoUI.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace GettingStartedWithKendoUI.Web.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() {
+            ViewBag.Title = "Hello World";
+            return View();
+        }
+    }
+}
